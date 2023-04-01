7c4e929c2781ff628ef4d2093e0be45f15c7ae9a
Ariel D'Alessandro <ariel.dalessandro@gmail.com>
2023-04-01 17:35:16 -0300

    components/layout-components: Run prettier
    Signed-off-by: Ariel D'Alessandro <ariel.dalessandro@gmail.com>

--- src/components/layout-components/MobileNav.js
+++ src/components/layout-components/MobileNav.js
@@ -1,53 +1,49 @@
 import React from "react";
 import { Drawer } from "antd";
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from "react-redux";
 import { NAV_TYPE_SIDE } from "constants/ThemeConstant";
 import { Scrollbars } from "react-custom-scrollbars-2";
 import MenuContent from "./MenuContent";
-import { onMobileNavToggle } from 'store/slices/themeSlice';
+import { onMobileNavToggle } from "store/slices/themeSlice";
 import Logo from "./Logo";
 import Flex from "components/shared-components/Flex";
 import { ArrowLeftOutlined } from "@ant-design/icons";
 
-export const MobileNav = ({
-	routeInfo,
-	hideGroupTitle
-}) => {
+export const MobileNav = ({ routeInfo, hideGroupTitle }) => {
+  const dispatch = useDispatch();
 
-	const dispatch = useDispatch();
+  const sideNavTheme = useSelector((state) => state.theme.sideNavTheme);
+  const mobileNav = useSelector((state) => state.theme.mobileNav);
 
-	const sideNavTheme = useSelector(state => state.theme.sideNavTheme);
-	const mobileNav = useSelector(state => state.theme.mobileNav);
+  const menuContentprops = { sideNavTheme, routeInfo, hideGroupTitle };
 
-	const menuContentprops = { sideNavTheme, routeInfo, hideGroupTitle };
+  const onClose = () => {
+    dispatch(onMobileNavToggle(false));
+  };
 
-	const onClose = () => {
-		dispatch(onMobileNavToggle(false));
-	};
-
-	return (
-		<Drawer
-			placement="left"
-			closable={false}
-			onClose={onClose}
-			open={mobileNav}
-			bodyStyle={{ padding: 5 }}
-		>
-			<Flex flexDirection="column" className="h-100">
-				<Flex justifyContent="space-between" alignItems="center">
-					<Logo mobileLogo={true} />
-					<div className="nav-close" onClick={() => onClose()}>
-						<ArrowLeftOutlined />
-					</div>
-				</Flex>
-				<div className="mobile-nav-menu">
-					<Scrollbars autoHide>
-						<MenuContent type={NAV_TYPE_SIDE} {...menuContentprops} />
-					</Scrollbars>
-				</div>
-			</Flex>
-		</Drawer>
-	);
+  return (
+    <Drawer
+      placement="left"
+      closable={false}
+      onClose={onClose}
+      open={mobileNav}
+      bodyStyle={{ padding: 5 }}
+    >
+      <Flex flexDirection="column" className="h-100">
+        <Flex justifyContent="space-between" alignItems="center">
+          <Logo mobileLogo={true} />
+          <div className="nav-close" onClick={() => onClose()}>
+            <ArrowLeftOutlined />
+          </div>
+        </Flex>
+        <div className="mobile-nav-menu">
+          <Scrollbars autoHide>
+            <MenuContent type={NAV_TYPE_SIDE} {...menuContentprops} />
+          </Scrollbars>
+        </div>
+      </Flex>
+    </Drawer>
+  );
 };
 
 export default MobileNav;
--- src/components/layout-components/NavItem.js
+++ src/components/layout-components/NavItem.js
@@ -1,44 +1,46 @@
-import styled from '@emotion/styled';
-import { 
-    GRAY_SCALE,
-} from 'constants/ThemeConstant'
-import { baseTheme, rgba } from 'configs/ThemeConfig'
+import styled from "@emotion/styled";
+import { GRAY_SCALE } from "constants/ThemeConstant";
+import { baseTheme, rgba } from "configs/ThemeConfig";
 
 const isDark = (mode) => {
-    return mode === 'dark'
-}
+  return mode === "dark";
+};
 
-const NavItem = styled('div')`
-    display: flex;
-    align-items: center;
-    line-height: 1.5;
-    cursor: pointer;
-    padding: 0 1rem;
-    color: ${props => isDark(props.mode) ?  GRAY_SCALE.GRAY : rgba(GRAY_SCALE.WHITE, 0.85) };
+const NavItem = styled("div")`
+  display: flex;
+  align-items: center;
+  line-height: 1.5;
+  cursor: pointer;
+  padding: 0 1rem;
+  color: ${(props) =>
+    isDark(props.mode) ? GRAY_SCALE.GRAY : rgba(GRAY_SCALE.WHITE, 0.85)};
 
-    .ant-badge {
-        color: ${props => isDark(props.mode) ?  GRAY_SCALE.GRAY : rgba(GRAY_SCALE.WHITE, 0.85) };
+  .ant-badge {
+    color: ${(props) =>
+      isDark(props.mode) ? GRAY_SCALE.GRAY : rgba(GRAY_SCALE.WHITE, 0.85)};
+  }
+
+  .nav-icon {
+    font-size: 1.25rem;
+  }
+
+  &:hover,
+  &.ant-dropdown-open,
+  &.ant-popover-open {
+    > * {
+      color: ${(props) =>
+        isDark(props.mode) ? baseTheme.colorPrimary : GRAY_SCALE.WHITE};
     }
 
-    .nav-icon {
-        font-size: 1.25rem;
+    .profile-text {
+      color: ${(props) =>
+        isDark(props.mode) ? GRAY_SCALE.GRAY : GRAY_SCALE.WHITE};
     }
+  }
+`;
 
-    &:hover,
-    &.ant-dropdown-open,
-    &.ant-popover-open {
-        > * {
-            color: ${props => isDark(props.mode) ? baseTheme.colorPrimary : GRAY_SCALE.WHITE};
-        }
+export default NavItem;
 
-        .profile-text {
-            color: ${props => isDark(props.mode) ? GRAY_SCALE.GRAY : GRAY_SCALE.WHITE};
-        }
-    }
-`
-
-export default NavItem
-
-// @media ${device.laptop} { 
+// @media ${device.laptop} {
 //     max-width: 800px;
 // }
