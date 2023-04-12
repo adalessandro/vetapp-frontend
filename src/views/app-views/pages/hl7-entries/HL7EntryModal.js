277b8ae208376450dd779201c8852bbf925c4c29
Ariel D'Alessandro <ariel.dalessandro@gmail.com>
2023-04-12 13:24:35 -0300

    Add app-views/pages/hl7-entries/HL7EntryModal.js
    Signed-off-by: Ariel D'Alessandro <ariel.dalessandro@gmail.com>

---
+++ src/views/app-views/pages/hl7-entries/HL7EntryModal.js
@@ -0,0 +1,108 @@
+import React from "react";
+import { Modal, Descriptions, Table } from "antd";
+
+function genTable(data) {
+  const ret = [];
+
+  function genChildTriplet(id, title) {
+    const child = {
+      key: id,
+      title: title,
+      value: data[`${id}Value`],
+      unit: data[`${id}Unit`],
+      range: data[`${id}Range`],
+    };
+    ret.push(child);
+  }
+
+  genChildTriplet("wbc", "WBC");
+  genChildTriplet("lymP", "LYM%");
+  genChildTriplet("granP", "GRAN%");
+  genChildTriplet("midP", "MID%");
+  genChildTriplet("lymN", "LYM#");
+  genChildTriplet("granN", "GRAN%");
+  genChildTriplet("midN", "MID#");
+  genChildTriplet("rbc", "RBC");
+  genChildTriplet("hgb", "HGB");
+  genChildTriplet("hct", "HCT");
+  genChildTriplet("mcv", "MCV");
+  genChildTriplet("mch", "MCH");
+  genChildTriplet("mchc", "MCHC");
+  genChildTriplet("rdwcv", "RDW-CV");
+  genChildTriplet("rdwsd", "RDW-SD");
+  genChildTriplet("plt", "PLT");
+  genChildTriplet("mpv", "MPV");
+  genChildTriplet("pdw", "PDW");
+  genChildTriplet("pct", "PCT");
+
+  return ret;
+}
+
+const HL7EntryModal = ({ data, visible, close }) => {
+  if (!data) return;
+
+  const handleOk = () => {
+    close();
+  };
+
+  const handleCancel = () => {
+    close();
+  };
+
+  const headerData = [
+    { key: "patientAlias", title: "Paciente" },
+    { key: "patientSex", title: "Sexo" },
+    { key: "resultAge", title: "Edad" },
+    { key: "patientRace", title: "Especie" },
+    { key: "patientName", title: "Responsable" },
+    { key: "observationCollector", title: "Solicita" },
+  ];
+
+  const tableColumns = [
+    {
+      title: "Campo",
+      dataIndex: "title",
+      key: "title",
+    },
+    {
+      title: "Valor",
+      dataIndex: "value",
+      key: "value",
+    },
+    {
+      title: "Unidad",
+      dataIndex: "unit",
+      key: "unit",
+    },
+    {
+      title: "Rango",
+      dataIndex: "range",
+      key: "range",
+    },
+  ];
+  const tableData = genTable(data);
+
+  return (
+    <>
+      <Modal
+        title={`Muestra #${data.id} - (${new Date(
+          data.observationDate
+        ).toLocaleString()})`}
+        open={visible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <Descriptions title="" layout="vertical">
+          {headerData.map((header) => (
+            <Descriptions.Item label={header.title} key={header.title}>
+              {data[header.key]}
+            </Descriptions.Item>
+          ))}
+        </Descriptions>
+        <Table size="small" columns={tableColumns} dataSource={tableData} />
+      </Modal>
+    </>
+  );
+};
+
+export default HL7EntryModal;
--- src/views/app-views/pages/hl7-entries/index.js
+++ src/views/app-views/pages/hl7-entries/index.js
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
 import { Card, Table, Tooltip, message, Button } from "antd";
-import { DeleteOutlined, UnorderedListOutlined } from "@ant-design/icons";
+import {
+  DeleteOutlined,
+  EyeOutlined,
+  UnorderedListOutlined,
+} from "@ant-design/icons";
 import HL7EntryView from "./HL7EntryView";
+import HL7EntryModal from "./HL7EntryModal";
 import HL7EntryService from "../../../../services/HL7EntryService";
+import utils from "utils";
 
 export class HL7EntryList extends Component {
   state = {
     hl7Entries: [],
     hl7Visible: false,
+    hl7ModalVisible: false,
     selectedHL7Entry: null,
   };
 
@@ -45,15 +52,86 @@ export class HL7EntryList extends Component {
     });
   };
 
+  showHL7EntryModal = (hl7Entry) => {
+    this.setState({
+      hl7ModalVisible: true,
+      selectedHL7Entry: hl7Entry,
+    });
+  };
+
+  closeHL7EntryModal = () => {
+    this.setState({
+      hl7ModalVisible: false,
+      selectedHL7Entry: null,
+    });
+  };
+
   render() {
-    const { hl7Entries, hl7Visible, selectedHL7Entry } = this.state;
+    const { hl7Entries, hl7Visible, hl7ModalVisible, selectedHL7Entry } =
+      this.state;
 
     const tableColumns = [
       {
-        title: "Id",
+        title: "#",
         dataIndex: "id",
         sorter: {
-          compare: (a, b) => a.role.id - b.role.id,
+          compare: (a, b) => utils.antdTableSorter(a, b, "id"),
+        },
+      },
+      {
+        title: "Fecha",
+        dataIndex: "observationDate",
+        render: (a) => new Date(a).toLocaleString(),
+        sorter: {
+          compare: (a, b) => {
+            return (
+              Date.parse(a.observationDate) - Date.parse(b.observationDate)
+            );
+          },
+        },
+      },
+      {
+        title: "Paciente",
+        dataIndex: "patientAlias",
+        sorter: {
+          compare: (a, b) => utils.antdTableSorter(a, b, "patientAlias"),
+        },
+      },
+      {
+        title: "Sexo",
+        dataIndex: "patientSex",
+        sorter: {
+          compare: (a, b) => utils.antdTableSorter(a, b, "patientSex"),
+        },
+      },
+      {
+        title: "Edad",
+        dataIndex: "resultAge",
+        sorter: {
+          compare: (a, b) => utils.antdTableSorter(a, b, "id"),
+        },
+      },
+      {
+        title: "Especie",
+        dataIndex: "patientRace",
+        sorter: {
+          compare: (a, b) => utils.antdTableSorter(a, b, "patientRace"),
+        },
+      },
+      {
+        title: "Responsable",
+        dataIndex: "patientName",
+        sorter: {
+          compare: (a, b) => utils.antdTableSorter(a, b, "patientName"),
+        },
+      },
+
+      {
+        title: "Solicita",
+        dataIndex: "observationCollector",
+        sorter: {
+          compare: (a, b) =>
+            utils.antdTableSorter(a, b, "observationCollector"),
         },
       },
       {
@@ -68,6 +146,18 @@ export class HL7EntryList extends Component {
                 icon={<UnorderedListOutlined />}
                 onClick={() => {
                   this.showHL7Entry(elm);
+                }}
+                size="small"
+              />
+            </Tooltip>
+
+            <Tooltip title="Modal">
+              <Button
+                type="primary"
+                className="mr-2"
+                icon={<EyeOutlined />}
+                onClick={() => {
+                  this.showHL7EntryModal(elm);
                 }}
                 size="small"
               />
@@ -97,6 +187,13 @@ export class HL7EntryList extends Component {
           visible={hl7Visible}
           close={() => {
             this.closeHL7Entry();
+          }}
+        />
+        <HL7EntryModal
+          data={selectedHL7Entry}
+          visible={hl7ModalVisible}
+          close={() => {
+            this.closeHL7EntryModal();
           }}
         />
       </Card>
